03f858aaaf6981f032bc8da38c8d2a8dc600eaae
Leonardo Borges <leoborgesaf@hotmail.com>
2025-01-28 21:53:41 -0300

    feat/filter-lang

--- api-github/src/pages/RepositoriesPage/Filter/index.jsx
+++ api-github/src/pages/RepositoriesPage/Filter/index.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Clear, Container, Selector } from "./styles";
 
 function Filter() {
+  const langs = [
+    { name: "Javascript", count: 5, color: "#f1c40f" },
+    { name: "Python", count: 5, color: "#a9a4df" },
+    { name: "Typescript", count: 5, color: "#1702fa" },
+  ];
+
+  const selectors = langs.map((lang) => (
+    <Selector key={lang.name.toLowerCase()} color={lang.color}>
+      <span>{lang.name}</span>
+      <span>{lang.count}</span>
+    </Selector>
+  ));
 
   return (
-    <div>Filter</div>
-  )
+    <Container>
+      {selectors}
+      <Clear>Reset</Clear>
+    </Container>
+  );
 }
 
-export default Filter
+export default Filter;
