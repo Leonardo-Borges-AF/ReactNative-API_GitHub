e13e8ca4d48f1e4355bb83b3462d52b26a5f95a6
Leonardo Borges <leoborgesaf@hotmail.com>
2025-01-28 23:01:51 -0300

    fix/Profile-API

--- api-github/src/pages/RepositoriesPage/Profile/index.jsx
+++ api-github/src/pages/RepositoriesPage/Profile/index.jsx
@@ -1,33 +1,50 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
+import { MdGroup, MdLocationCity, MdWork } from "react-icons/md";
 
-import {MdGroup, MdLocationCity, MdWork} from 'react-icons/md';
+import { Avatar, Container, Data, Header, Inner, Login, Name } from "./styles";
 
-import { Avatar, Container, Data, Header, Inner, Login, Name } from './styles'
-
-function Profile() {
+function Profile({ user }) {
   return (
     <Container>
       <Header>
-        <Avatar src='https://avatars.githubusercontent.com/u/78118687?v=4'/>
-        <Login> Login </Login>
-        <Name> Leonardo Borges </Name>
+        <Avatar src={user.avatar_url} />
+        <Login> {user.login} </Login>
+        <Name> {user.name} </Name>
       </Header>
       <Inner>
         <Data>
-          <MdGroup size={20}/>
-          x <i>seguidores</i> &middot; y <i>seguindot</i>
+          <MdGroup size={20} />
+          {user.followers}&nbsp;<i>seguidores</i> &nbsp;&middot;&nbsp;{" "}
+          {user.following}&nbsp;
+          <i>seguindo</i>
         </Data>
-        <Data> 
-          <MdWork size={20}/> 
-          Front-End/Web3
-        </Data>
-        <Data> 
-          <MdLocationCity size={20}/> 
-          Ribeirão Preto
+        {user.company && (
+          <Data>
+            <MdWork size={20} />
+            {user.company}
+          </Data>
+        )}
+        <Data>
+          <MdLocationCity size={20} />
+          {user.location}
         </Data>
       </Inner>
     </Container>
-  )
+  );
 }
 
-export default Profile
+Profile.propTypes = {
+  user: PropTypes.shape({
+    login: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    avatar_url: PropTypes.string.isRequired,
+    followers: PropTypes.number.isRequired,
+    following: PropTypes.number.isRequired,
+    company: PropTypes.string,
+    blog: PropTypes.string,
+    location: PropTypes.string,
+  }).isRequired,
+};
+
+export default Profile;
--- api-github/src/pages/RepositoriesPage/index.jsx
+++ api-github/src/pages/RepositoriesPage/index.jsx
@@ -6,10 +6,20 @@ import Filter from "./Filter";
 import Repositories from "./Repositories";
 
 function RepositoriesPage() {
+  const user = {
+    login: "Leonardo-Borges-AF",
+    avatar_url: "https://avatars.githubusercontent.com/u/78118687?v=4",
+    followers: 4,
+    following: 4,
+    company: null,
+    blog: "",
+    location: "Bandeirantes - PR",
+    name: "Leonardo Borges Arias Ferreira",
+  };
   return (
     <Container>
       <SideBar>
-        <Profile />
+        <Profile user={user} />
         <Filter />
       </SideBar>
       <Main>
